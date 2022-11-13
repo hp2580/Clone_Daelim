cd0bd4c9f9edd2bebf29f9618e9d5a203f244ed9
hp2580 <happycandy0909@gmail.com>
2022-11-13 20:50:27 +0900

    section1 drag event add
    slide is modified. It can be moved by user drag & buttons

--- asserts/js/section1.js
+++ asserts/js/section1.js
@@ -1,6 +1,7 @@
 let sec1_wrap = document.querySelector(".sec1_wrap");
 let paginations = document.querySelectorAll(".pagination li");
 let sec1_Pages = document.querySelectorAll(".sec1");
+let sec1_index = 0;
 
 for (let pagination of paginations) {
   pagination.addEventListener("click", () => {
@@ -10,25 +11,60 @@ for (let pagination of paginations) {
     pagination.classList.add("active");
     switch (class_Pagination) {
       case "sec1_p1":
-        sec1_Pages[0].classList.add("active");
-        sec1_wrap.style.transform = `translateX(0)`;
+        sec1_index = 0;
+        // sec1_Pages[0].classList.add("active");
+        // sec1_wrap.style.transform = `translateX(0)`;
         break;
       case "sec1_p2":
-        sec1_Pages[1].classList.add("active");
-        sec1_wrap.style.transform = `translateX(-20%)`;
+        sec1_index = 1;
+        // sec1_Pages[1].classList.add("active");
+        // sec1_wrap.style.transform = `translateX(-20%)`;
         break;
       case "sec1_p3":
-        sec1_Pages[2].classList.add("active");
-        sec1_wrap.style.transform = `translateX(-40%)`;
+        sec1_index = 2;
+        // sec1_Pages[2].classList.add("active");
+        // sec1_wrap.style.transform = `translateX(-40%)`;
         break;
       case "sec1_p4":
-        sec1_Pages[3].classList.add("active");
-        sec1_wrap.style.transform = `translateX(-60%)`;
+        sec1_index = 3;
+        // sec1_Pages[3].classList.add("active");
+        // sec1_wrap.style.transform = `translateX(-60%)`;
         break;
       default:
-        sec1_Pages[4].classList.add("active");
-        sec1_wrap.style.transform = `translateX(-80%)`;
+        sec1_index = 4;
+        // sec1_Pages[4].classList.add("active");
+        // sec1_wrap.style.transform = `translateX(-80%)`;
         break;
     }
+    //   sec1_Pages[sec1_index].classList.add("active");
+    //   sec1_wrap.style.transform = `translateX(-${20 * sec1_index}%)`;
+    sec1(sec1_index);
   });
+}
+
+let sec1_prevPoint;
+let sec1_nextPoint;
+sec1_wrap.addEventListener("mousedown", (e) => {
+  sec1_prevPoint = e.screenX;
+});
+
+sec1_wrap.addEventListener("mouseup", (e) => {
+  sec1_nextPoint = e.screenX;
+  let sec1_direction = sec1_nextPoint - sec1_prevPoint;
+  if (sec1_direction > 0) {
+    clearActive(paginations);
+    clearActive(sec1_Pages);
+    sec1_index = sec1_index > 0 ? sec1_index - 1 : 4;
+  } else if (sec1_direction < 0) {
+    clearActive(paginations);
+    clearActive(sec1_Pages);
+    sec1_index = sec1_index < 4 ? sec1_index + 1 : 0;
+  }
+  sec1(sec1_index);
+});
+
+function sec1(index) {
+  paginations[index].classList.add("active");
+  sec1_Pages[index].classList.add("active");
+  sec1_wrap.style.transform = `translateX(-${20 * index}%)`;
 }
